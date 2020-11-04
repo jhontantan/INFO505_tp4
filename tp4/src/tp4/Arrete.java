f5302c7986b0c1717d1740b2d864e63a849f8c57
jhontantan <jomar404@gmail.com>
2020-11-04 10:12:06 +0100

    arrete v1.1

--- tp4/src/tp4/Arrete.java
+++ tp4/src/tp4/Arrete.java
@@ -7,7 +7,11 @@ public class Arrete {
 	private double distance;
 	private double qtePhe;
 	
-	public Arrete() {
+	public Arrete(Ville ville1, Ville ville2, double distance, double qtePhe ) {
+		this.ville1=ville1;
+		this.ville2=ville2;
+		this.distance=distance;
+		this.qtePhe=qtePhe;
 		
 		
 	}
